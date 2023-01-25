0d778942799d032be08cc22b052a8c3ada0a4d88
Mhatons <mhatons@gmail.com>
2023-01-25 13:13:43 +0100

    resolving the dir issue

--- route/api_route.js
+++ route/api_route.js
@@ -20,7 +20,8 @@ let storage = multer.diskStorage({
         // fs.writeFileSync("/tmp/users.json", JSON.stringify(users))
         
         const filePath = path.join("/tmp", "../public/uploads");
-        fs.writeFileSync(filePath, JSON.stringify(data));
+        cb(null, filePath)
+        // fs.writeFileSync(filePath, JSON.stringify(data));
 
         // let __dir = path.join(__dirname, "../public/uploads")
         // cb(null, __dir)
