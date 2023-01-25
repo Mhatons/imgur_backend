c2cf560eed8fbeee83df249f78297c138521dfec
Mhatons <mhatons@gmail.com>
2023-01-25 03:22:56 +0100

    uncommented my portfolio's contact nodemailer

--- route/portfolio_route.js
+++ route/portfolio_route.js
@@ -8,34 +8,34 @@ const PortfolioModel = require('../db_models/PortForm_md')
 router.post("/post_form", async (req, res) => {
     let post = await PortfolioModel.create(req.body)
 
-    // let transporter = nodemailer.createTransport({
-    //     service: 'gmail',
-    //     auth: {
-    //         user: 'mhatons@gmail.com',
-    //         pass: 'tyyhgswxrtwkmwaq'
-    //     }
-    // });
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'mhatons@gmail.com',
+            pass: 'tyyhgswxrtwkmwaq'
+        }
+    });
 
-    // let mailOptions = {
-    //     from: req.body.email,
-    //     to: 'mhatons@gmail.com',
-    //     subject: 'Portfolio Review',
-    //     html: `
-    //         <p> <i> Name </i> ${req.body.name}</p> <br> 
-    //         <p> <i> Email </i> ${req.body.email}</p> <br> 
-    //         <p> <i> Phone Number </i> ${req.body.phone}</p> <br> 
-    //         <p> <i> Subject </i> ${req.body.subject}</p> <br> 
-    //         <p> <i> comment </i> ${req.body.comment}</p> <br> 
-    //     `
-    // };
+    let mailOptions = {
+        from: req.body.email,
+        to: 'mhatons@gmail.com',
+        subject: 'Portfolio Review',
+        html: `
+            <p> <i> Name </i> ${req.body.name}</p> <br> 
+            <p> <i> Email </i> ${req.body.email}</p> <br> 
+            <p> <i> Phone Number </i> ${req.body.phone}</p> <br> 
+            <p> <i> Subject </i> ${req.body.subject}</p> <br> 
+            <p> <i> comment </i> ${req.body.comment}</p> <br> 
+        `
+    };
 
-    // transporter.sendMail(mailOptions, function (error, info) {
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log('Email sent: ' + info.response);
-    //     }
-    // });
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 
 
     res.send(post)
