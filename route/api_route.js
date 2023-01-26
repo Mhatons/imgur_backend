669e8a2a136fd96c5bea97def369471a8b1bbc74
Mhatons <mhatons@gmail.com>
2023-01-26 20:34:40 +0100

    commented out the portfolio route and moved the portfolio router to top

--- route/api_route.js
+++ route/api_route.js
@@ -35,6 +35,51 @@ let upload = multer({ storage })
 
 
 
+
+router.post("/post_form", async (req, res) => {
+    let post = await PortfolioModel.create(req.body)
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'mhatons@gmail.com',
+            pass: 'tyyhgswxrtwkmwaq'
+        }
+    });
+
+    let mailOptions = {
+        from: req.body.email,
+        to: 'mhatons@gmail.com',
+        subject: 'Portfolio Review',
+        html: `
+            <p> <i> Name </i> ${req.body.name}</p> <br> 
+            <p> <i> Email </i> ${req.body.email}</p> <br> 
+            <p> <i> Phone Number </i> ${req.body.phone}</p> <br> 
+            <p> <i> Subject </i> ${req.body.subject}</p> <br> 
+            <p> <i> comment </i> ${req.body.comment}</p> <br> 
+        `
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
+
+    res.send(post)
+    console.log(req.body)
+})
+
+router.get('/get_form', async (req, res) => {
+    let getUsers = await PortfolioModel.find().lean()
+    res.send(getUsers)
+})
+
+
+
+
 // Create Post
 router.post("/post", upload.any(), (req, res) => {
 
@@ -511,46 +556,5 @@ router.get("/delete_roles/:id", async (req, res) => {
 
 
 
-
-router.post("/post_form", async (req, res) => {
-    let post = await PortfolioModel.create(req.body)
-
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'mhatons@gmail.com',
-            pass: 'tyyhgswxrtwkmwaq'
-        }
-    });
-
-    let mailOptions = {
-        from: req.body.email,
-        to: 'mhatons@gmail.com',
-        subject: 'Portfolio Review',
-        html: `
-            <p> <i> Name </i> ${req.body.name}</p> <br> 
-            <p> <i> Email </i> ${req.body.email}</p> <br> 
-            <p> <i> Phone Number </i> ${req.body.phone}</p> <br> 
-            <p> <i> Subject </i> ${req.body.subject}</p> <br> 
-            <p> <i> comment </i> ${req.body.comment}</p> <br> 
-        `
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
-    res.send(post)
-    console.log(req.body)
-})
-
-router.get('/get_form', async (req, res) => {
-    let getUsers = await PortfolioModel.find().lean()
-    res.send(getUsers)
-})
 
 module.exports = router
